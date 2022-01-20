d54a21381105adac7ebd1cf2968300da6fb5cd89
Hridoy Hossain <ridoy.java@gmail.com>
2022-01-20 21:07:13 +0600

    bangladesh fragment

---
+++ app/src/main/java/com/example/quickquiz/Bangladesh.java
@@ -0,0 +1,14 @@
+package com.example.quickquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bangladesh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bangladesh);
+    }
+}
